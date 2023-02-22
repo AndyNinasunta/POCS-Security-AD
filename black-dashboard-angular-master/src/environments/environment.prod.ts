c915e41c478df57007cccf2bc2b7dd92838bb607
Andy Ninasunta <andy2000-09@hotmail.com>
2023-02-21 23:32:03 -0500

    Segundo commit

--- black-dashboard-angular-master/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true, codeAd: 'ABC345_APP2',
-  urlApi: 'https://localhost:7234/api/v1/Security/'
-};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  codeAd: "ABC345_APP2",
+  urlApi: "https://localhost:7234/api/v1/Security/",
+};
